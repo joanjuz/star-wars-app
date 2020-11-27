59ce367c31e131e0646dbd722dcfaffe285e0e74
joanjuz <62675444+joanjuz@users.noreply.github.com>
2020-11-27 17:59:26 -0600

    Alert

---
+++ src/Alert.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Alert extends React.Component{
+    render(){
+        return (
+            <div>{this.props.count > 60 ? "Peligro! la cantidad de segundos ha excedido el limite!!!" : ""}</div>
+        );
+    }
+}
